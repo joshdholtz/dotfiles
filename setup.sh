d8c892ea4a86da34116eb13db049c80ce4975adc
Josh Holtz <josh@rokkincat.com>
2020-07-24 13:29:09 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+# Ruby
+echo 'ruby 2.5.3' >> ~/.tool-versions
+
+# asdf - version manager
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
 echo  '. $HOME/.asdf/asdf.sh' >> ~/.bashrc 
+asdf install
+
