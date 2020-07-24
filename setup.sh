b89022b6ab945c904aa422c92bdd2e75c65cda0c
Josh Holtz <josh@rokkincat.com>
2020-07-24 13:44:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,14 @@ echo 'ruby 2.5.3' >> ~/.tool-versions
 # asdf - version manager
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
 echo  '. $HOME/.asdf/asdf.sh' >> ~/.bashrc 
-asdf install
 
+asdf plugin add ruby
+asdf install
+asdf global ruby 2.5.3
+
+gem install bundler
+bundle install
+
+alias be='bundle exec'
+alias rspec='be rspec'
+alias fl='be fastlane'
